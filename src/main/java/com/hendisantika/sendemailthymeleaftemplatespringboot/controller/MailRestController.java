bcabaed1560e208c926fe6f743fdbe2f2f20d986
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 05:53:50 +0700

    #10 - Add sendMail API in MailRestController

--- src/main/java/com/hendisantika/sendemailthymeleaftemplatespringboot/controller/MailRestController.java
+++ src/main/java/com/hendisantika/sendemailthymeleaftemplatespringboot/controller/MailRestController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.sendemailthymeleaftemplatespringboot.controller;
 
+import com.hendisantika.sendemailthymeleaftemplatespringboot.model.Mail;
 import com.hendisantika.sendemailthymeleaftemplatespringboot.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +30,12 @@ public class MailRestController {
 
     @Autowired
     private MailService mailService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMail(@Valid @RequestBody Mail mail, Errors errors) {
+        if (errors.hasErrors()) {
+            return new ResponseEntity<>(errors.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+        return mailService.sendMail(mail);
+    }
 }
