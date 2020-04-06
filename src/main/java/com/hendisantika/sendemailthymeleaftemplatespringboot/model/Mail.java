1a6cccecd06f17a13fb5e0393684dc81c5773507
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 05:51:02 +0700

    #4 - Add Mail Model

---
+++ src/main/java/com/hendisantika/sendemailthymeleaftemplatespringboot/model/Mail.java
@@ -0,0 +1,33 @@
+package com.hendisantika.sendemailthymeleaftemplatespringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : send-email-thymeleaf-template-spring-boot
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/20
+ * Time: 05.50
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mail {
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\" +
+            ".[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    private String email;
+
+    @NotNull
+    private String object;
+
+    @NotNull
+    private String message;
+}
