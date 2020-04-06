397244d7d69aaf6f02ed62851240a9c1c8258d98
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 05:48:07 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/sendemailthymeleaftemplatespringboot/SendEmailThymeleafTemplateSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.sendemailthymeleaftemplatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailThymeleafTemplateSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendEmailThymeleafTemplateSpringBootApplication.class, args);
+    }
+
+}
