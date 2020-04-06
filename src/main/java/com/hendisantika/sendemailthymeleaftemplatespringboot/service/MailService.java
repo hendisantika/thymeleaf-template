e4f8913803c19f65f3112fb4f83e7c58aa83d1d1
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 05:51:49 +0700

    #5 - Add Mail Service

---
+++ src/main/java/com/hendisantika/sendemailthymeleaftemplatespringboot/service/MailService.java
@@ -0,0 +1,28 @@
+package com.hendisantika.sendemailthymeleaftemplatespringboot.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : send-email-thymeleaf-template-spring-boot
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/20
+ * Time: 05.51
+ */
+@Service
+public class MailService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MailService.class);
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+}
